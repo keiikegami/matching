c4218ba3fc255f035c1753d56ac6e734c7b381cb
ikegamikei <sybaster.x@gmail.com>
2015-06-30 17:25:16 +0900

    Noneに対応できたのか怪しい...

--- deferred_acceptance.py
+++ deferred_acceptance.py
@@ -1,71 +1,124 @@
-def da3(P,Q):
-    import numpy as np
-    import sys
+import numpy as np
+def deferred_acceptance(m_pref_input,f_pref_input,caps):
     
-    if isinstance(Q, list) != True and isinstance(Q,np.ndarray) != True:
-        print "Input Error!!"
-        sys.exit()
-        
-    if isinstance(P, list):
-        P = np.array(P)
-        Q = np.array(Q)
-    elif isinstance(P, np.ndarray):
-        print "ndarray"
-    else:
-        print "Input Error!!"
-        sys.exit()
-    N=P.shape[0]
-    G=Q.shape[0]
-    S=np.tile([-1],N)
-    free=range(N)
-    n=0
+    m_pref_array=np.array(m_pref_input)
+    f_pref_array=np.array(f_pref_input)
+    f_prefsort=np.argsort(f_pref_array)
+    m_popu=m_pref_array.shape[0]
+    f_popu=f_pref_array.shape[0]
     
-    while free:
-        #print S
-        #print n
-        A=free.pop() 
-        F=P[A,:][n]
-        if F==G:
-            S[A]=G
-            n=0
-        else:
-            for J in range(N+1):   
-                if Q[F,:][J]==A:
-                    D=J
-                if Q[F,:][J]==N:
-                    E=J
-            if E<D:
-                free.append(A)
-                n+=1
+    if caps == None:
+        caps = [1] * f_popu
+    
+    caps_array=np.array(caps)
+    
+    
+    free=list(range(m_popu))
+    
+    
+    m_match=np.tile([f_popu],m_popu)
+
+   
+    f_accept = {}
+    for i in range(f_popu):
+        f_accept[i] = []
+            
+    
+    
+    while len(free) > 0:
+        applyman = free.pop()
+        print applyman
+        apply_prefs=m_pref_array[applyman]
+        print apply_prefs
+       
+        for prefered in apply_prefs:
+            print prefered
+            if prefered == f_popu:
+                m_match[applyman]=f_popu
+                print m_match
+                break
+            
             else:
-                if F not in S:
-                    S[A]=F
-                    n=0        
-                else:
-                    for y in range(N):
-                        if S[y]==F:
-                            M=y         
-                    for z in range(N+1):
-                        if Q[F,:][z]==M:
-                            Z=z
-                        if Q[F,:][z]==A:
-                            W=z
-                    if W<Z:
-                        S[A]=F
-                        S[M]=-1
-                        free.append(M)
-                        n=0
+                prefered_pref = f_prefsort[prefered]
+                print prefered_pref
+                rank_apply = prefered_pref[applyman]
+                rank_non_match = prefered_pref[m_popu]
+                
+                if rank_non_match > rank_apply:
+                    prefered_accept = f_accept[prefered]
+                    print prefered_accept
+                    
+                    if len(prefered_accept) < caps[prefered]:
+                        
+                        if len(prefered_accept) == 0:
+                            prefered_accept.append(applyman)
+                            print prefered_accept
+                            m_match[applyman] = prefered
+                            print m_match
+                            break
+                        
+                
+                        else:
+                            for i in range(len(prefered_accept)):
+                                
+                                rank_already = prefered_pref[prefered_accept[i]]
+                                m_match[applyman] = prefered
+                                print m_match
+                                
+                                if rank_already > rank_apply:
+                                    prefered_accept.insert(i,applyman)
+                                    print prefered_accept
+                                    break
+                                    
+                                else:
+                                    if i == len(prefered_accept)-1:
+                                        prefered_accept.append(applyman)
+                                        print prefered_accept
+                            break
+                     
+                 
                     else:
-                        free.append(A)
-                        n+=1
-        if len(free)==0:   
-            s=list(S)
-            L=np.tile([N],G)
-            l=list(L)
-            for u in s:
-                if u==G: continue  
-                else:
-                    v=s.index(u)
-                    l[u]=v
-            return s,l
-        else: continue
+                        for i in range(len(prefered_accept)): 
+                            rank_already = prefered_pref[prefered_accept[i]]
+                            
+                            if rank_already > rank_apply: 
+                                
+                                prefered_accept.insert(i,applyman)
+                                
+                                unmatch_man = prefered_accept.pop()
+                                print unmatch_man
+                                m_match[unmatch_man]=f_popu
+                                m_match[applyman] = prefered
+                                print m_match
+                                free.append(unmatch_man)
+                                print free
+                                print prefered_accept
+                                break
+                                    
+                                
+                        if applyman in prefered_accept:
+                            break
+                        
+                        else:
+                            continue
+                            
+                
+    for i in range(f_popu):
+        f_indi = f_accept[i]
+        gap = caps[i] - len(f_indi)
+        
+        if gap > 0:
+            residual = [m_popu] * gap
+            f_indi.extend(residual)
+    
+ 
+    f_match = []
+    for i in range(f_popu):
+        f_match.extend(f_accept[i])
+        
+    
+    indptr = np.cumsum(caps)
+    l_indptr = list(indptr)
+    l_indptr.insert(0,0)
+
+    return [list(m_match),f_match,l_indptr]
